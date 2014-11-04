6d76c55f3fc049d54621f65008f28118c80725a9
wardogsq <wardogsq@hotmail.com>
2014-11-04 12:16:09 -0500

    Version 6

--- NetflixUninterrupter.user.js
+++ NetflixUninterrupter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Netflix Uninterrupter
 // @namespace    wardogsq@hotmail.com
-// @version      0.5
+// @version      0.6
 // @description  Watch uninterrupted netflix!
 // @author       Luke Seraphin
 // @match        http://www.netflix.com/WiPlayer*
@@ -20,9 +20,10 @@ video.play();
 var interrupter = document.getElementsByClassName('player-autoplay-interrupter')[0];
 var controls = document.getElementsByClassName('player-controls-wrapper')[0];
 var backbutton = document.getElementsByClassName('player-back-to-browsing')[0];
-interrupter.style.display="none";
-controls.className = controls.className + " opacity-transparent";
-backbutton.className = controls.className + " opacity-transparent";
-    
+var cinema = document.getElementById('netflix-player');
+    try{interrupter.style.display="none";}catch(err){console.log(err);}
+try{controls.className = controls.className + " opacity-transparent";}catch(err){console.log(err);}
+try{backbutton.className = backbutton.className + " opacity-transparent";}catch(err){console.log(err);}
+try{cinema.className = cinema.className + " player-cinema-mode";}catch(err){console.log(err);}
 }
             }, 600);
