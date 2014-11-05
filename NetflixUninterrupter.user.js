73d36b63d8d8e2bef0261882110e58bce60d51b2
wardogsq <wardogsq@hotmail.com>
2014-11-05 02:54:16 -0500

    Final Version
    Flawless

--- NetflixUninterrupter.user.js
+++ NetflixUninterrupter.user.js
@@ -7,23 +7,8 @@
 // @match        http://www.netflix.com/WiPlayer*
 // @grant		 none
 // ==/UserScript==
-
 setInterval(function(){
-    var interrupterShow = false;
-    if(document.getElementsByClassName('player-autoplay-interrupter')[0]){
-    if(document.getElementsByClassName('player-autoplay-interrupter')[0].style.display!="none"){interrupterShow=true;}
-        }
-    
-if(interrupterShow){
-var video = document.getElementsByTagName('video')[0];
-video.play();
-var interrupter = document.getElementsByClassName('player-autoplay-interrupter')[0];
-var controls = document.getElementsByClassName('player-controls-wrapper')[0];
-var backbutton = document.getElementsByClassName('player-back-to-browsing')[0];
-var cinema = document.getElementById('netflix-player');
-    try{interrupter.style.display="none";}catch(err){console.log(err);}
-try{controls.className = controls.className + " opacity-transparent";}catch(err){console.log(err);}
-try{backbutton.className = backbutton.className + " opacity-transparent";}catch(err){console.log(err);}
-try{cinema.className = cinema.className + " player-cinema-mode";}catch(err){console.log(err);}
+if(document.getElementsByClassName('player-autoplay-interrupter')[0]){
+document.getElementsByClassName('button continue-playing')[0].click();
 }
-            }, 600);
+}, 600);
